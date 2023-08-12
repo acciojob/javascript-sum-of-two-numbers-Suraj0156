04029e96b5107ff3b672c9c5fbe01763bb3ea993
Suraj0156 <130788586+Suraj0156@users.noreply.github.com>
2023-08-12 13:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 let input1 = prompt("Enter the first number:");
 let input2 = prompt("Enter the second number:");
+function sumOfTwoNumbers(a, b) {
+  //Write your code here
+   let sum = a + b;
+  console.log(sum);
+
+  
+}
+
+
+
+
+/*Do not change the code below */
+const rl = require("readline");
+
+const reader = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let a, b;
+
+reader.on("line", (line) => {
+  if (!a) {
+    a = parseInt(line);
+  } else if (!b) {
+    b = parseInt(line);
+    sumOfTwoNumbers(a, b)
+    reader.close();
+  } else {
+    // we already have both variables, do nothing
+  }
+});
+
 
 // Write your code here and print the output using alert function
